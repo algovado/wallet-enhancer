11976b8cdb8dff8fd3091707f1d373befd7e2948
algovado <90692255+algovado@users.noreply.github.com>
2023-12-17 01:14:58 +0300

    Delete src directory

--- src/components/selects/SelectSubHeader.tsx
+++
@@ -1,8 +0,0 @@
-import ListSubheader, { ListSubheaderProps } from "@mui/material/ListSubheader";
-
-function SelectSubHeader(props: ListSubheaderProps) {
-  return <ListSubheader {...props} />;
-}
-
-SelectSubHeader.muiSkipListHighlight = true;
-export default SelectSubHeader;
--- src/core/modules.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "uint8-to-base64" {
-  export function encode(uint8: Uint8Array): string;
-  export function decode(base64: string): Uint8Array;
-}
