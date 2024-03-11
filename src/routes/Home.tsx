21419f48168b5fd11cafcceb93cceb442c594834
algovado <90692255+algovado@users.noreply.github.com>
2024-03-11 18:34:56 +0300

    add donation dialog to home

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -205,6 +205,7 @@ export default function Home() {
         <div className="mt-4 mx-auto sm:invisible">
           <SearchWalletInput />
         </div>
+        <DonationDialog/>
       </main>
     );
   }
